abbc6d501b920ee393cc8999661b325b1b49072d
khleoshih <b02901086@ntu.edu.tw>
2017-05-01 20:32:14 +0800

    Update accel_input.js

--- input/accel_input.js
+++ input/accel_input.js
@@ -6,25 +6,22 @@ var io = require('socket.io')(http);
 var port = process.env.PORT || 3000;
 
 app.get('/', function(req, res){
-  res.sendFile(__dirname + '/index.html');
+	res.sendFile(__dirname + '/index.html');
 });
 
-//io.on('connection', function(socket){
-	var data = new Object();
-	accel.on('ready', function () {
-		accel.on('data', function (xyz) {
-			data.x = xyz[0].toFixed(2);
-			data.y = xyz[1].toFixed(2);
-			data.z = xyz[2].toFixed(2);
-			console.log(data.x);
-			io.emit('accel', data);
-		});		
-	});
+var data = new Object();
+accel.on('ready', function () {
+	accel.on('data', function (xyz) {
+		data.x = xyz[0].toFixed(2);
+		data.y = xyz[1].toFixed(2);
+		data.z = xyz[2].toFixed(2);
+		io.emit('accel', data);
+	});		
+});
 
-	accel.on('error', function(err){
-  		console.log('Error:', err);
-	});
-//});
+accel.on('error', function(err){
+	console.log('Error:', err);
+});
 
 http.listen(port, function(){
   console.log('listening on *:' + port);
